42bcbc11605a7cd4d85b929e810e83922959d753
enhopkin@gmail.com <enhopkin@gmail.com>
2022-05-04 09:58:55 -0600

    working on search with links to filter results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,137 +1,134 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import SearchByName from './SearchByName'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchByName from "./SearchByName";
 
 const initialForm = {
-    name: '',
-    category: '',
-    area: '',
-    searchType: '',
-}
+  name: "",
+  category: "",
+  area: "",
+  searchType: "",
+};
+let data;
+let id = 0;
 
-function SearchBar () {
-    const [form, setForm] = useState(initialForm)
-    const [categoryList, setCategoryList] = useState([])
-    const [selectMealList, setSelectMealList] = useState([])
-    const [selectMeal, setSelectMeal] = useState([])
+const getId = () => ++id;
 
- 
-    useEffect(() => {
-        if(form.searchType==='category'){
-            return fetchCategoryList()
-        }
-    }, [form])
+function SearchBar() {
+  const [form, setForm] = useState(initialForm);
+  const [list, setList] = useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [areaList, setAreaList] = useState([]);
+  const [selectMealList, setSelectMealList] = useState([]);
+  const [selectMeal, setSelectMeal] = useState([]);
 
-    
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  console.log(form);
 
-    const fetchCategoryList =() => {
-        axios
-            .get(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-            .then(res => {
-                setCategoryList(res.data.categories)
-                setSelectMealList([])
-            })
-            .catch(err => {
-                console.log ({err})
-            })
-    }
-
-    const onChange = (e) => {
-        const {name, value} =e.target;
-        setForm({
-            ...form,
-            [name]: value
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (form.searchType === "name") {
+      axios
+        .get(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${form.name}`
+        )
+        .then((res) => {
+          setList(res.data.meals);
+          console.log(res);
         })
-    }
-    // console.log(categoryList)
-
-    const onSubmit = e => {
-        e.preventDefault()
-        axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${form.name}`)
-                .then(res => {
-                    setSelectMealList(res.data.meals)
-                    setCategoryList([])
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log({err})
-                })
-        }
-
-    const mealSelection =(id) => {
-        setSelectMeal(selectMealList.filter(meal => {
-            return id===meal.id
+        .catch((err) => {
+          console.log({ err });
+        });
+    } else if (form.searchType === "category") {
+      axios
+        .get("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then((res) => {
+          setList(res.data.categories);
         })
-         )
-         console.log('working')
+        .catch((err) => {
+          console.log({ err });
+        });
+    } else {
+      axios
+        .get("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+        .then((res) => {
+          setList(res.data.meals);
+        })
+        .catch((err) => {
+          console.log({ err });
+        });
     }
-    console.log (selectMeal)
+  };
+  console.log(list);
 
-    return(
-        <div className = 'search-component'>
-            <form onSubmit = {onSubmit}>
-                <label>SEARCH TYPE
-                    <select 
-                    name= 'searchType'
-                    type= 'search-type'
-                    onChange = {onChange}
-                    value = {form.searchType}
-                    >
-                    <option value=''> -Select- </option>
-                    <option value='name'>Name</option>
-                    <option value= 'category'>Category</option>
-                    <option value= 'area'>Area</option>
-                    </select>
-                </label>
-                <div>
-                {
-                form.searchType==='name' &&
-                    // <SearchByName form = {form} onChange = {onChange}/>
-                <label>
-                    <input 
-                    name = 'name'
-                    type = 'text'
-                    placeholder='search by name'
-                    onChange = {onChange}
-                    value = {form.name}
-                    />
-                </label>
-                }
-                {
-                form.searchType==='category' &&
-               <> 
-               <select 
-                    type= 'category'
-                    onChange = {onChange}
-                    value= {categoryList}
-                >
-                    
-                    {categoryList.map(category => {
-                        return(
-                        <option 
-                        onClick = {mealSelection}
-                        key = {category.idCategory}
-                        name ={category.strCategory}
-                        onChange = {onChange}
-                        // value= {categoryList.category}
-                        >{category.strCategory}</option>
-                        
-                        )}
-                    )}
-                    </select>
-                    
-                    </>
-                }
-                </div>
-                <button>Search</button>
-            </form>
-            <ul>
-            {selectMealList.map(meal => 
-                <li onClick = {mealSelection} key = {meal.idMeal}>{meal.strMeal}</li>)}
-            </ul>
+  const categorySearch =(strCategory) => {
+      axios 
+        .get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${strCategory}`)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log({err})
+        })
+  }
 
+  // const mealSelection =(id) => {
+  //     setSelectMeal(selectMealList.filter(meal => {
+  //         return id===meal.id
+  //     })
+  //      )
+  //      console.log('working')
+  // }
+  // console.log (selectMeal)
+
+  return (
+    <div className="search-component">
+      <form onSubmit={onSubmit}>
+        <label>
+          SEARCH TYPE
+          <select
+            name="searchType"
+            type="search-type"
+            onChange={onChange}
+            value={form.searchType}
+          >
+            <option value=""> -Select- </option>
+            <option value="name">Name</option>
+            <option value="category">Category</option>
+            <option value="area">Area</option>
+          </select>
+        </label>
+        <div>
+          {form.searchType === "name" && (
+            <label>
+              <input
+                name="name"
+                type="text"
+                placeholder="search by name"
+                onChange={onChange}
+                value={form.name}
+              />
+            </label>
+          )}
         </div>
-    )
+        <button> SUBMIT </button>
+      </form>
+
+      <ul>
+        {form.searchType === "name" &&
+          list.map((meal) => <li key={meal.idMeal}>{meal.strMeal}</li>)}
+        {form.searchType === "category" &&
+          list.map((cat) => <li onClick = {categorySearch(cat.strCategory)} key={cat.idCategory}>{cat.strCategory}</li>)}
+        {form.searchType === "area" &&
+          list.map((place) => <li key={getId}> {place.strArea}</li>)}
+      </ul>
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
