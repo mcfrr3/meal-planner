4a569da1af4e79e1e79e4569e2625281aa58fb67
Irving Delgado <rvngdelgado@gmail.com>
2022-05-04 11:46:29 -0500

    completed Sources page and functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import logo from '../logo.svg';
 import '../App.css';
 
+// import SearchBar from './Search';
 import RandomMeal from './RandomMeal';
+import Sources from './SourcePage'
 
 function App() {
   return (
     <div className="App">
       <RandomMeal />
+      {/* <SearchBar /> */}
+      <Sources />
     </div>
   );
 }
