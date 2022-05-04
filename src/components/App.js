551fb5849b4a56e96ab23a0d68a198b88fe401d8
Irving Delgado <rvngdelgado@gmail.com>
2022-05-04 12:07:49 -0500

    Start of Ingredients component

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,16 @@ import logo from '../logo.svg';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-<<<<<<< HEAD
-// import SearchBar from './Search';
-=======
 import SearchBar from './Search';
->>>>>>> 85f2bde811e8978110fdc3249c215e2f32761eb4
 import RandomMeal from './RandomMeal';
 import Sources from './SourcePage'
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <RandomMeal />
-      {/* <SearchBar /> */}
+      <SearchBar />
       <Sources />
-=======
-      <div className='container'>
-        <RandomMeal />
-        <SearchBar/>
-      </div>
->>>>>>> 85f2bde811e8978110fdc3249c215e2f32761eb4
     </div>
   );
 }
